d7632a37a51a050a1417279da23920dff7a03a5e
Mr. Web Developer <117938690+Yahyobek-Coder@users.noreply.github.com>
2023-11-01 00:12:24 +0500

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -30,8 +30,10 @@ api_hash = "1e6ca420184a701db1f8a1301df99288"
 # Input for the string session
 string = input("Press enter to continue: ")
 
-# Initialize the Telegram client
-client = TelegramClient(StringSession(string), api_id, api_hash)
+#
+string_session = str(client.session.save())
+print("String session:", string_session)
+
 
 # Input for the phone number or bot token
 phone_number = input("Please enter your phone number (or bot token): ")
